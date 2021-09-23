f68aec82d68a23440d6d25febfa05ec8495ad163
伊欧 <langyo.china@gmail.com>
2021-09-23 23:12:45 +0800

    ✨ 加入最小化的登录代码。

--- gulpfile.ts
+++ gulpfile.ts
@@ -59,64 +59,6 @@ export const taskBuild = series(
         })
       )
       .pipe(dest(join(__dirname, './dist/')));
-  },
-
-  function webpackBuildForFrontend() {
-    return src(join(__dirname, './src/server/index.ts'))
-      .pipe(
-        webpack({
-          module: {
-            rules: [
-              {
-                test: /\.[jt]sx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                  presets: [
-                    '@babel/preset-env',
-                    [
-                      '@babel/preset-react',
-                      {
-                        runtime: 'automatic',
-                        importSource: '@emotion/react',
-                      },
-                    ],
-                    '@babel/preset-typescript',
-                  ],
-                  plugins: [
-                    '@babel/plugin-transform-runtime',
-                    '@emotion/babel-plugin',
-                  ],
-                },
-              },
-              {
-                test: /\.s[ac]ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-              },
-            ],
-          },
-          resolve: {
-            extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx'],
-            modules: [join(__dirname, './node_modules'), 'node_modules'],
-          },
-          resolveLoader: {
-            modules: [join(__dirname, './node_modules'), 'node_modules'],
-          },
-          mode:
-            process.env.NODE_ENV === 'development'
-              ? 'development'
-              : 'production',
-          target: 'node',
-          output: {
-            filename: 'server.bundle.js',
-          },
-          devtool:
-            process.env.NODE_ENV === 'production'
-              ? 'none'
-              : 'inline-source-map',
-        })
-      )
-      .pipe(dest(join(__dirname, './dist/')));
   }
 );
 
---
+++ src/server/index.ts
@@ -0,0 +1,32 @@
+import { createClient } from 'oicq';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const { qq, key } = JSON.parse(
+  readFileSync(join(process.cwd(), './system-config.json'), 'utf8')
+);
+const client = createClient(qq);
+
+//监听上线事件
+client.on('system.online', () => console.log('Logged in!'));
+
+//监听消息并回复
+client.on('message', (event) => event.reply('hello world'));
+
+client
+  .on('system.login.slider', function (event) {
+    //监听滑动验证码事件
+    process.stdin.once('data', (input) => {
+      const ticket = input.toString('utf8').trim();
+      console.log('接收到的 ticket:');
+      console.log(ticket);
+      this.sliderLogin(ticket); //输入ticket
+    });
+  })
+  .on('system.login.device', function (event) {
+    //监听登录保护验证事件
+    process.stdin.once('data', () => {
+      this.login(); //验证完成后按回车登录
+    });
+  })
+  .login(key); //需要填写密码或md5后的密码
